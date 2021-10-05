e3ae68a668d6456f192312283239f3dc397a5f7c
vively <alebymars@ya.ru>
2021-10-06 02:26:53 +0700

    lab3

---
+++ App.cpp
@@ -0,0 +1,55 @@
+#include "App.h"
+
+void App::start() {
+
+	List list;
+
+	list.getListFromFile(const_cast<char*>("Date.txt"));
+
+	int key = -1;
+	do {
+
+		cout << "\n  _________________________________________"
+			<< "\n |                                         |"
+			<< "\n |        ** Выберите операцию **          |"
+			<< "\n |  1) Показать все даты                   |"
+			<< "\n |  2) Показать дату - след.дату           |"
+			<< "\n |  3) Показать системную дату и время     |"
+			<< "\n |  0) Выйти                               |"
+			<< "\n |_________________________________________|"
+			<< "\n  > ";
+		cin >> key;
+
+		switch (key)
+		{
+		case 1: {
+
+			list.listPrint();
+
+			break;
+		}
+
+		case 2: {
+
+			list.listPrintSort();
+
+			break;
+		}
+
+		case 3: {
+			//Вывод системной даты
+			datetime sysTime;
+			sysTime.showSysDate();
+			break;
+		}
+
+		default:
+
+			if (key != 0) cout << "\n\nОшибка, номер не найден. Попробуйте еще раз." << endl;
+
+			break;
+		}
+
+	} while (key != 0);
+
+}
---
+++ lab3.cpp
@@ -0,0 +1,8 @@
+﻿#include "App.h"
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    App app;
+    app.start();
+    return 0;
+}
